5988636ee643734ebf0f83a411e0692da1f3d2e4
Spencer <spencer.adler2@gmail.com>
2024-05-28 14:22:22 -0400

    added click event listener and edited attributes of the JSON file

--- index.js
+++ index.js
@@ -23,12 +23,29 @@ const displayDogs = () => {
     let img = document.createElement('img')
     img.src = dog.image
     img.id = 'dog-images'
+    let dogName = document.createElement('h3')
+    dogName.innerText = `Name: ${dog.name}`
+    let dogBreed = document.createElement('h3')
+    dogBreed.innerText = `Breed: ${dog.breed}`
     document.querySelector('#dog-list').appendChild(img)
+    document.querySelector('#dog-list').appendChild(dogName)
+    document.querySelector('#dog-list').appendChild(dogBreed)
   }
+
+  function handleButton(){
+    alert('You have been added the waitlist!! We will email you shortly.')
+  }
+
 
   const renderPage = () => {
     displayDogs()
   }
+
+//   function handleDoubleClick(){
+//     img.src = dogsArray[0].image2
+// }
   
   //Event Listeners
   document.addEventListener("DOMContentLoaded", renderPage)
+  btn.addEventListener('click', handleButton)
+//   img.addEventListener('dblclick', handleDoubleClick)
