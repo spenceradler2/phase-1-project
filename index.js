4ea898cba2acbdd0654d11aa8a5af9e505b5df97
Spencer <spencer.adler2@gmail.com>
2024-05-28 16:02:16 -0400

    Edited double click event listener to show the correct dog when doubleclicking

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const displayDogs = () => {
         renderDog(dog)
       })
     }
-  };
+  }
 
   function renderDog(dog){
     let img = document.createElement('img')
@@ -30,23 +30,27 @@ const displayDogs = () => {
     document.querySelector('#dog-list').appendChild(img)
     document.querySelector('#dog-list').appendChild(dogName)
     document.querySelector('#dog-list').appendChild(dogBreed)
+    img.dataset.id = dog.id
     img.addEventListener('dblclick', handleDoubleClick)
-    function handleDoubleClick(){
-        img.src = dogsArray[0].image2
+
+
+    function handleDoubleClick(e){
+        let id = e.target.dataset.id
+        let dogImage2 = dogsArray.find(dog => dog.id == id)
+        img.src = dogImage2.image2
     }
+    
+
 }
 
   function handleButton(){
     alert('You have been added the waitlist!! We will email you shortly.')
   }
 
-
   const renderPage = () => {
     displayDogs()
   }
 
-
-  
   //Event Listeners
   document.addEventListener("DOMContentLoaded", renderPage)
   btn.addEventListener('click', handleButton)
